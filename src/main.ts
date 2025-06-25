9ba2511904ce15927bf26e1a3398186cc7eb17bf
Khanh261 <giakhanh006@gmail.com>
2025-06-25 09:28:36 +0700

    auto deploy check

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,8 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   const config = new DocumentBuilder()
-    .setTitle('money example')
-    .setDescription('The money API description')
+    .setTitle('samba example')
+    .setDescription('The samba API description')
     .setVersion('1.0')
     .addTag('cats')
     .build();
