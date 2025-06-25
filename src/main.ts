0c4e39fbd8777a51c6f2129c314096b0073fbfc9
Khanh261 <giakhanh006@gmail.com>
2025-06-25 09:20:48 +0700

    test deploy

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,8 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   const config = new DocumentBuilder()
-    .setTitle('Dogs example')
-    .setDescription('The cats API description')
+    .setTitle('money example')
+    .setDescription('The money API description')
     .setVersion('1.0')
     .addTag('cats')
     .build();
